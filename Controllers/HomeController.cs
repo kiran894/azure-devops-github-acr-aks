96b82d7224fcfc910c7dba437572ee0ec7a1f806
Reddy <Kiran.Reddy@finastra.com>
2021-06-21 22:02:54 -0400

    1st commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dotnetGrafana.Models;
+using Prometheus;
+
+namespace dotnetGrafana.Controllers
+{
+   
+    
+    public class HomeController : Controller
+    {
+        Counter counter = Metrics.CreateCounter("my_counter", "index page counter");
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            counter.Inc();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
